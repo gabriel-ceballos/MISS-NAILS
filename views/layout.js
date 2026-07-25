83d03979daabe5ec70cebd07b000e2c051a6b871
Gabriel Ceballos <ceballosgg@hotmail.com>
2026-07-24 23:42:58 -0600

    migrar puente a api

---
+++ views/layout.js
@@ -0,0 +1,53 @@
+const layout = {
+
+    mostrar() {
+
+        const app = document.getElementById("app");
+
+        app.innerHTML = `
+
+<div class="layout">
+
+    <header id="header"></header>
+
+    <main id="contenido"></main>
+
+</div>
+
+`;
+
+    },
+
+    header() {
+
+        document.getElementById("header").innerHTML = `
+
+<div class="barra-superior">
+
+    <div class="logo">
+
+        <img
+            src="img/logo.png"
+            alt="Miss Nails">
+
+    </div>
+
+    <div class="usuario">
+
+        Hola ${sesion.usuario.nombre}
+
+    </div>
+
+</div>
+
+`;
+
+    },
+
+    contenido(html){
+
+        document.getElementById("contenido").innerHTML = html;
+
+    }
+
+};
