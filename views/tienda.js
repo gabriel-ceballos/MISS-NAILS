75a21595c737fe0ee7e5ef15fe130ae00e6ad185
Gabriel Ceballos <ceballosgg@hotmail.com>
2026-07-26 22:42:55 -0600

    Migrar puente a api

--- views/tienda.js
+++ views/tienda.js
@@ -1,103 +1,85 @@
 const tienda = {
 
-    mostrar() {
+    async mostrar() {
 
         layout.mostrar();
 
         layout.header();
 
+        const lista = await productos.obtener();
+
+        let htmlProductos = "";
+
+        lista.forEach(producto => {
+
+            htmlProductos += this.crearTarjeta(producto);
+
+        });
+
         layout.contenido(`
 
 <div class="tienda">
 
+    <div class="categorias">
 
+        <button class="categoria activa">Todas</button>
+        <button class="categoria">Geles</button>
+        <button class="categoria">Rubber</button>
+        <button class="categoria">Acrílicos</button>
+        <button class="categoria">Top Coat</button>
+        <button class="categoria">Promociones</button>
 
-<div class="categorias">
-
-    <button class="categoria activa">Todas</button>
-
-    <button class="categoria">Geles</button>
-
-    <button class="categoria">Rubber</button>
-
-    <button class="categoria">Acrílicos</button>
-
-    <button class="categoria">Top Coat</button>
-
-    <button class="categoria">Promociones</button>
-
-</div>
+    </div>
 
     <div class="productos">
 
-        <div class="producto">
-
-            <div class="foto"></div>
-
-            <h3>Gel Pink</h3>
-
-            <p class="precio">$250 MXN</p>
-
-    <button class="btnAgregar">
-
-        🛒 Agregar
-
-    </button>
-        </div>
-
-        <div class="producto">
-
-            <div class="foto"></div>
-
-            <h3>Rubber Base</h3>
-
-            <p class="precio">$180 MXN</p>
-
-    <button class="btnAgregar">
-
-        🛒 Agregar
-
-    </button>
-
-        </div>
-
-        <div class="producto">
-
-            <div class="foto"></div>
-
-            <h3>Top Coat</h3>
-
-             <p class="precio">$150 MXN</p>
-
-    <button class="btnAgregar">
-
-        🛒 Agregar
-
-    </button>
-
-        </div>
-
-        <div class="producto">
-
-            <div class="foto"></div>
-
-            <h3>Acrílico Cover</h3>
-
-  <p class="precio">$320 MXN</p>
-
-    <button class="btnAgregar">
-
-        🛒 Agregar
-
-    </button>
-
-        </div>
+        ${htmlProductos}
 
     </div>
 
 </div>
 
 `);
+
+    },
+
+    crearTarjeta(producto){
+
+        return `
+
+<div class="producto">
+
+    <div class="foto">
+
+        <img
+            src="${producto.imagen || 'img/productos/sin-foto.png'}"
+            class="fotoProducto">
+
+    </div>
+
+    <h3>
+
+        ${producto.nombre}
+
+    </h3>
+
+    <p class="precio">
+
+        ${producto.precio}
+
+    </p>
+
+    <button
+        class="btnAgregar"
+        data-id="${producto.id}">
+
+        🛒 Agregar
+
+    </button>
+
+</div>
+
+`;
 
     }
 
