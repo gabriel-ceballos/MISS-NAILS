0ce9184e8a019e74e50fe0daa5acc2a91a2e484b
Gabriel Ceballos <ceballosgg@hotmail.com>
2026-08-15 21:50:54 -0600

    Refina icono de notificaciones mobile

--- views/mobile.js
+++ views/mobile.js
@@ -35,14 +35,27 @@ const mobile = {
 
                     </div>
 
-                    <button
-                        id="btnNotificaciones"
-                        class="mobile-notificaciones"
-                        type="button">
+<button
+    id="btnNotificaciones"
+    class="mobile-notificaciones"
+    aria-label="Notificaciones">
 
-                        🔔
+    <svg
+        class="icono-notificacion"
+        viewBox="0 0 24 24"
+        aria-hidden="true">
 
-                    </button>
+        <path
+            d="M18 8a6 6 0 0 0-12 0c0 7-3 7-3 9h18c0-2-3-2-3-9">
+        </path>
+
+        <path
+            d="M10 21h4">
+        </path>
+
+    </svg>
+
+</button>
 
                 </header>
 
